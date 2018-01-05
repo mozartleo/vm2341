b59a0a9f3b5f04968bffa4de5537127517e60edf
Mozart <mozart@syneforge.com>
2018-01-05 13:41:23 +0200

    Initial commit

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/")
+     */
     public function index()
     {
         $this->render('default/index.html.twig');
